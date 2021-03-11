ed0302bc1bee24a24131fd07d9d08a7a7ea7cf6c
Cristian Garibay <garibcri@oregonstate.edu>
2021-03-11 08:46:16 -0800

    added multiply test

--- test_calculator.py
+++ test_calculator.py
@@ -11,4 +11,5 @@ class TestCalculator:
     def test_subtract(self):
         assert 6 == calculator.subtract(8, 2)
 
-    
+    def test_multiply(self):
+        assert 9 == calculator.subtract(3,3)
